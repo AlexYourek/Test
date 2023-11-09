99fa13d61a16f6d174c66ec4183ff06042322f8f
Aleksander Jurek <alexanderj@wp.pl>
2023-11-09 11:31:23 +0100

    initial commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class BankAccount {
-}
+public class BankAccount {
+    /*Stwórz klasę BankAccount, która reprezentuje konto bankowe.
+Klasa powinna zawierać pola, takie jak numerKonta, saldo i wlasciciel,
+oraz konstruktor, który umożliwia tworzenie obiektów BankAccount z określonym numerem konta,
+saldem początkowym i właścicielem. Dodaj metody do wpłacania i wypłacania środków
+oraz wyświetlania aktualnego salda. Stwórz kilka kont bankowych,
+wykonaj operacje wpłaty i wypłaty, a następnie wyświetl ich saldo.*/
+private String accountNumber;
+private double balance;
+private String owner;
+
+    public BankAccount(String accountNumber, double startBalance, String owner) {
+        this.accountNumber = accountNumber;
+        this.balance = startBalance;
+        this.owner = owner;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("wpłacasz: " + amount);
+        } else {
+            System.out.println("zła kwota");
+        }
+    }
+
+        public void withdraw(double amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+                System.out.println("wypłacasz: " + amount);
+            } else {
+                System.out.println("zła kwota, lub brak środków");
+            }
+        }
+
+        public void displayBalance() {
+            System.out.println("saldo konta " + accountNumber + ": " + balance);
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BankAccount account1 = new BankAccount("1360 0000",5000,"Jan Kowalski");
+        BankAccount account2 = new BankAccount("9876 1111",1000,"Zofia Kowalska");
+        BankAccount account3 = new BankAccount("1360 2222",4000,"Józek Kowalski");
+
+        account1.displayBalance();
+        account2.displayBalance();
+        account3.displayBalance();
+
+        account1.deposit(100);
+        account2.withdraw(200);
+        account3.deposit(300);
+
+        account1.displayBalance();
+        account2.displayBalance();
+        account3.displayBalance();
+
     }
 }
